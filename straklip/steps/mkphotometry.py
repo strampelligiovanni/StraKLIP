db7b7c5bcac77030f9639c175cca5bd89a779f29
strampelligiovanni <strampelligiovanni@gmail.com>
2024-02-19 14:37:35 -0800

    removed flag classification from this step

--- straklip/steps/mkphotometry.py
+++ straklip/steps/mkphotometry.py
@@ -5,7 +5,7 @@ from concurrent.futures import ProcessPoolExecutor
 from itertools import repeat
 from ancillary import parallelization_package
 from utils_photometry import mvs_aperture_photometry,avg_aperture_photometry
-
+import os
 def get_ee_df(dataset):
     """
     Create the Encircled Energy Dataframe dictionary from file.
@@ -34,24 +34,20 @@ def get_ee_df(dataset):
 
 def task_mvs_photometry(DF,fitsname,ids_list,filter,ee_dict,zpt,la_cr_remove,cr_radius,multiply_by_exptime,
                         multiply_by_gain,multiply_by_PAM,bpx_list,spx_list,radius_in,radius1_in,radius2_in,sat_thr,
-                        kill_plots,grow_curves,r_in,p,gstep):
+                        kill_plots,grow_curves,r_in,p,gstep, path2savefile):
     '''
     parallelized task for the update_mvs_tiles.
     '''
     phot=[]
     for id in ids_list:
-        type_flag = DF.avg_targets_df.loc[DF.avg_targets_df.avg_ids == DF.crossmatch_ids_df.loc[
-            DF.crossmatch_ids_df.mvs_ids == id].avg_ids.unique()[0]].type.values[0]
-        flag = 'good_target'
-        if type_flag == 2:
-            flag = 'unresolved_double'
         phot.append(mvs_aperture_photometry(DF,filter,ee_dict,zpt,fitsname=fitsname,
                                             mvs_ids_list_in=[id],bpx_list=bpx_list,spx_list=spx_list,
                                             la_cr_remove=la_cr_remove,cr_radius=cr_radius,radius_in=radius_in,
                                             radius1_in=radius1_in,radius2_in=radius2_in,sat_thr=sat_thr,
                                             kill_plots=kill_plots,grow_curves=grow_curves,r_in=r_in,p=p,
-                                            gstep=gstep,flag=flag,multiply_by_exptime=multiply_by_exptime,
-                                            multiply_by_gain=multiply_by_gain,multiply_by_PAM=multiply_by_PAM))
+                                            gstep=gstep,multiply_by_exptime=multiply_by_exptime,
+                                            multiply_by_gain=multiply_by_gain,multiply_by_PAM=multiply_by_PAM,
+                                            path2savefile=path2savefile))
 
     return(phot)
 
@@ -59,7 +55,7 @@ def make_mvs_photometry(DF,filter,mvs_ids_test_list=[],ee_dict=None,workers=None
                       parallel_runs=True,la_cr_remove=False,cr_radius=3,chunksize = None,
                       multiply_by_exptime=False,multiply_by_gain=False,multiply_by_PAM=False, bpx_list=[], spx_list=[],
                       zpt=0,radius_in=10,radius1_in=10,radius2_in=15, sat_thr=np.inf, kill_plots=True,
-                      grow_curves=True, r_in=1,p=100,gstep=0.1,skip_flags=['rejected']):
+                      grow_curves=True, r_in=1,p=100,gstep=0.1,skip_flags=['rejected'],path2savefile=None):
     '''
     update the multi-visits dataframe with the tile photometry for each source
 
@@ -72,6 +68,10 @@ def make_mvs_photometry(DF,filter,mvs_ids_test_list=[],ee_dict=None,workers=None
     getLogger(__name__).info(f'Make photometry for multi-visits targets on filter {filter}')
     fits_dict = {}
     skip_IDs_list_list = DF.mvs_targets_df.loc[DF.mvs_targets_df[f'flag_{filter}'].isin(skip_flags)].mvs_ids.unique()
+    if path2savefile is not None:
+        if not os.path.exists(path2savefile):
+            os.makedirs(path2savefile)
+            getLogger(__name__).info(f'Making {path2savefile} directory')
 
     if len(mvs_ids_test_list) != 0:
         fitsname_test_list = DF.mvs_targets_df.loc[
@@ -102,7 +102,7 @@ def make_mvs_photometry(DF,filter,mvs_ids_test_list=[],ee_dict=None,workers=None
                                     repeat(multiply_by_exptime),
                                     repeat(multiply_by_gain), repeat(multiply_by_PAM), repeat(bpx_list), repeat(spx_list), repeat(radius_in), repeat(radius1_in),
                                     repeat(radius2_in), repeat(sat_thr),
-                                    repeat(kill_plots), repeat(grow_curves), repeat(r_in), repeat(p), repeat(gstep),
+                                    repeat(kill_plots), repeat(grow_curves), repeat(r_in), repeat(p), repeat(gstep), repeat(path2savefile),
                                   chunksize=chunksize):
 
 
@@ -114,8 +114,8 @@ def make_mvs_photometry(DF,filter,mvs_ids_test_list=[],ee_dict=None,workers=None
                       'm_%s' % filter, 'e_%s' % filter, 'spx_%s' % filter,
                       'bpx_%s' % filter, 'r_%s' % filter, 'rsky1_%s' % filter, 'rsky2_%s' % filter,
                       'sky_%s' % filter, 'esky_%s' % filter, 'nsky_%s' % filter,
-                      'grow_corr_%s' % filter]] = phot[elno, 2:-1].astype(float)
-                    DF.mvs_targets_df.loc[sel,['flag_%s'%filter]]=phot[elno,-1:]
+                      'grow_corr_%s' % filter]] = phot[elno, 2:].astype(float)
+                    # DF.mvs_targets_df.loc[sel,['flag_%s'%filter]]=phot[elno,-1:]
 
     else:
         for elno in range(len(fitsname_list)):
@@ -123,7 +123,7 @@ def make_mvs_photometry(DF,filter,mvs_ids_test_list=[],ee_dict=None,workers=None
                                 multiply_by_exptime,
                                 multiply_by_gain, multiply_by_PAM, bpx_list, spx_list, radius_in, radius1_in,
                                 radius2_in, sat_thr,
-                                kill_plots, grow_curves, r_in, p, gstep)
+                                kill_plots, grow_curves, r_in, p, gstep, path2savefile)
             phot=np.array(phot)
             for elno in range(len(phot)):
                 sel=(DF.mvs_targets_df.mvs_ids==phot[elno,0].astype(float))&(DF.mvs_targets_df.ext==phot[elno,1].astype(float))
@@ -177,13 +177,15 @@ def run(packet):
                             radius_in=dataset.pipe_cfg.mkphotometry['radius_in']/DF.pixscale,
                             radius1_in=dataset.pipe_cfg.mkphotometry['radius1_in']/DF.pixscale,
                             radius2_in=dataset.pipe_cfg.mkphotometry['radius2_in']/DF.pixscale,
-                            kill_plots=not dataset.pipe_cfg.mkphotometry['verbose'],
+                            kill_plots=not dataset.pipe_cfg.mkphotometry['debug'],
                             grow_curves=dataset.pipe_cfg.mkphotometry['grow_curves'],
                             p=dataset.pipe_cfg.mkphotometry['p'],
                             gstep=dataset.pipe_cfg.mkphotometry['gstep'],
                             bpx_list=dataset.pipe_cfg.mkphotometry['bad_pixel_flags'],
                             spx_list=dataset.pipe_cfg.mkphotometry['sat_pixel_flags'],
-                            skip_flags=dataset.pipe_cfg.mkphotometry['skip_flags'])
+                            skip_flags=dataset.pipe_cfg.mkphotometry['skip_flags'],
+                            path2savefile = dataset.pipe_cfg.paths['database'] + f'/targets_photometry_tiles/{filter}')
+
 
         make_median_photometry(DF,filter,
                                avg_ids_list=[],

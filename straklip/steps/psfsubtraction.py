2ffaa69685fc55f435e860d5f0c5113e731cc2c7
strampelligiovanni <strampelligiovanni@gmail.com>
2024-02-19 16:45:50 -0800

    changed label kmodes_list to kmodes

--- straklip/steps/psfsubtraction.py
+++ straklip/steps/psfsubtraction.py
@@ -1,18 +1,17 @@
 from stralog import getLogger
 from utils_straklip import perform_KLIP_PSF_subtraction_on_tiles
 
-def KLIP_PSF_subtraction(DF, filter, label, mvs_ids_list=[], kmodes_list=[], workers=None, parallel_runs=True,
+def KLIP_PSF_subtraction(DF, filter, label, mvs_ids_list=[], kmodes=[], workers=None, parallel_runs=True,
                          skip_flags=['rejected', 'known_double'],overwrite=False, chunksize=None):
     '''
     This is a wrapper for KLIP PSF subtraction step
 
     '''
-    DF.kmodes_list = kmodes_list
     perform_KLIP_PSF_subtraction_on_tiles(DF, filter, label,
                                           workers=workers,
                                           parallel_runs=parallel_runs,
                                           mvs_ids_list=mvs_ids_list,
-                                          kmodes_list=kmodes_list,
+                                          kmodes=kmodes,
                                           skip_flags=skip_flags,
                                           chunksize=chunksize,
                                           overwrite=overwrite)
@@ -31,7 +30,7 @@ def run(packet):
         KLIP_PSF_subtraction(DF, filter,
                              label=label,
                              mvs_ids_list=[],
-                             kmodes_list=dataset.pipe_cfg.psfsubtraction['kmodes_list'],
+                             kmodes=dataset.pipe_cfg.psfsubtraction['kmodes'],
                              workers=dataset.pipe_cfg.ncpu,
                              parallel_runs=dataset.pipe_cfg.psfsubtraction['parallel_runs'],
                              skip_flags=dataset.pipe_cfg.psfsubtraction['skip_flags'],

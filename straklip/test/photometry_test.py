3d0c26a98f09db1b64a2b75c1668fe5b2ebe79fd
strampelligiovanni <strampelligiovanni@gmail.com>
2024-05-31 12:54:41 -0400

    corecxted typos

--- straklip/test/photometry_test.py
+++ straklip/test/photometry_test.py
@@ -2,21 +2,22 @@ from straklip import config,input_tables
 from datetime import datetime
 from dataframe import DataFrame
 from mkphotometry import get_ee_df, make_mvs_photometry
+from klipphotometry import update_candidate_dataframe
 
 pipe_cfg='/Users/gstrampelli/PycharmProjects/Giovanni/work/pipeline_logs/FFP/pipe.yaml'
 data_cfg='/Users/gstrampelli/PycharmProjects/Giovanni/work/pipeline_logs/FFP/data.yaml'
 pipe_cfg = config.configure_pipeline(pipe_cfg,pipe_cfg=pipe_cfg,data_cfg=data_cfg,dt_string=datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
 data_cfg = config.configure_data(data_cfg,pipe_cfg)
 
+
 dataset = input_tables.Tables(data_cfg, pipe_cfg)
-DF = DataFrame(path2out=pipe_cfg.paths['out'])
-DF.load_dataframe()
+DF = config.configure_dataframe(dataset,load=True)
 
 zpt = DF.zpt
 ee_dict=get_ee_df(dataset)
 for filter in ['f814w']:
     make_mvs_photometry(DF, filter,
-                        mvs_ids_test_list=[199],
+                        mvs_ids_test_list=[208],
                         ee_dict=ee_dict,
                         workers=1,
                         parallel_runs=False,
@@ -29,11 +30,12 @@ for filter in ['f814w']:
                         radius_in=dataset.pipe_cfg.mkphotometry['radius_in']/DF.pixscale,
                         radius1_in=dataset.pipe_cfg.mkphotometry['radius1_in']/DF.pixscale,
                         radius2_in=dataset.pipe_cfg.mkphotometry['radius2_in']/DF.pixscale,
-                        kill_plots=not dataset.pipe_cfg.mkphotometry['debug'],
+                        kill_plots=False,
                         grow_curves=dataset.pipe_cfg.mkphotometry['grow_curves'],
                         p=dataset.pipe_cfg.mkphotometry['p'],
                         gstep=dataset.pipe_cfg.mkphotometry['gstep'],
                         bpx_list=dataset.pipe_cfg.mkphotometry['bad_pixel_flags'],
                         spx_list=dataset.pipe_cfg.mkphotometry['sat_pixel_flags'],
                         skip_flags=dataset.pipe_cfg.mkphotometry['skip_flags'],
-                        path2savefile=dataset.pipe_cfg.paths['database']+f'/targets_photometry_tiles/{filter}')
+                        path2savefile=dataset.pipe_cfg.paths['out']+f'/targets_photometry_tiles/{filter}')
+

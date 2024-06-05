71f6387978c57b82a362300ea00da179ee581089
strampelligiovanni <strampelligiovanni@gmail.com>
2024-06-05 13:38:51 -0400

    added function to create fake injection dataframes

--- straklip/steps/buildhdf.py
+++ straklip/steps/buildhdf.py
@@ -1,6 +1,7 @@
 from stralog import getLogger
-from utils_dataframe import mk_crossmatch_ids_df,mk_avg_targets_df,mk_mvs_targets_df,mk_avg_candidates_df,mk_mvs_candidates_df
-def mk_targets_df(DF,dataset):#,multiple_visit_id_label='id',average_visits_id_label='id',avg_coords_labels=['ra','dec'],average_type_label='type',max_separation=2):
+from utils_dataframe import mk_crossmatch_ids_df,mk_avg_targets_df,mk_mvs_targets_df,mk_avg_candidates_df,mk_mvs_candidates_df,mk_fakes_df
+
+def mk_targets_df(DF,dataset):
     '''
     This is a wrapper for the creation of the targets dataframe
 
@@ -10,17 +11,40 @@ def mk_targets_df(DF,dataset):#,multiple_visit_id_label='id',average_visits_id_l
     mk_avg_targets_df(DF,dataset)
 
 def make_candidates_dataframes(DF):
-        '''
-        This is a wrapper for the creation of the candidates dataframes for the pipeline
+    '''
+    This is a wrapper for the creation of the candidates dataframes for the pipeline
 
-        Returns
-        -------
-        None.
+    Returns
+    -------
+    None.
 
-        '''
-        getLogger(__name__).info(f'Creating the candidates dataframe')
-        mk_avg_candidates_df(DF)
-        mk_mvs_candidates_df(DF)
+    '''
+    getLogger(__name__).info(f'Creating the candidates dataframe')
+    mk_avg_candidates_df(DF)
+    mk_mvs_candidates_df(DF)
+
+def make_fk_dataframes(DF,dataset):
+    '''
+    This is a wrapper for the creation of basic dataframes for the pipeline
+
+    Parameters
+    ----------
+
+
+    Returns
+    -------
+    None.
+
+    '''
+    getLogger(__name__).info(f'Creating the fake injection dataframe')
+    mk_fakes_df(DF,
+                MagBin_list=dataset.pipe_cfg.buildhdf['fp_table']['magbins'],
+                Dmag_list=dataset.pipe_cfg.buildhdf['fp_table']['dmags'],
+                Sep_range=dataset.pipe_cfg.buildhdf['fp_table']['sep_range'],
+                Nstar=dataset.pipe_cfg.buildhdf['fp_table']['nstar'],
+                filters=dataset.data_cfg.filters,
+                skip_filters=dataset.pipe_cfg.buildhdf['fp_table']['skip_filters'])
+
 
 def run(packet):
     dataset = packet['dataset']
@@ -29,5 +53,6 @@ def run(packet):
     mk_crossmatch_ids_df(DF, dataset.crossmatch_ids_table)
     mk_targets_df(DF,dataset)
     make_candidates_dataframes(DF)
+    make_fk_dataframes(DF,dataset)
     DF.save_dataframes(__name__)
 

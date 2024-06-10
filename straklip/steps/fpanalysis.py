17692051641cb2ebf4c5ef689a229925606b7078
strampelligiovanni <strampelligiovanni@gmail.com>
2024-06-10 11:14:48 -0400

    removed update_candidates_photometry and false_positive_analysis

---
+++ straklip/steps/fpanalysis.py
@@ -0,0 +1,14 @@
+# DF=update_candidates_photometry(DF,
+#                                 avg_ids_list=[],
+#                                 label=label,
+#                                 aptype=aptype,
+#                                 verbose=verbose,
+#                                 noBGsub=noBGsub,
+#                                 sigma=sigma,
+#                                 DF_fk=DF_fk,
+#                                 delta=delta,
+#                                 skip_filters=skip_filters,
+#                                 sat_thr=sat_thr,
+#                                 suffix=suffix)
+
+# DF=false_positive_analysis(avg_ids_list=[], showplot=True, verbose=True)

dbf011d747f5441fe8e2f29a64bb3d770dd06082
strampelligiovanni <strampelligiovanni@gmail.com>
2024-06-10 11:15:20 -0400

    removed update_candidates_photometry and false_positive_analysis

--- straklip/steps/mkcompleteness.py
+++ straklip/steps/mkcompleteness.py
@@ -100,20 +100,7 @@ def run_false_positive_analysis(DF,path2savedir=None, filters_list=[], Nvisit_ra
                             Kmodes_list=None,
                             ticks=ticks)
 
-    # DF=update_candidates_photometry(DF,
-    #                                 avg_ids_list=[],
-    #                                 label=label,
-    #                                 aptype=aptype,
-    #                                 verbose=verbose,
-    #                                 noBGsub=noBGsub,
-    #                                 sigma=sigma,
-    #                                 DF_fk=DF_fk,
-    #                                 delta=delta,
-    #                                 skip_filters=skip_filters,
-    #                                 sat_thr=sat_thr,
-    #                                 suffix=suffix)
 
-    # DF=false_positive_analysis(avg_ids_list=[], showplot=True, verbose=True)
     return(DF)
 
 def run(packet):

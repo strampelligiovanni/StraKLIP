4ca1b8ed3bbda8bac6ef7860842107e3523384a9
gstrampelli <strampelligiovanni@gmail.com>
2025-06-13 13:30:29 -0400

    initial commitment

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='StraKLIP',
-    version='0.1.0',
+    version='1.1.1',
     packages=find_packages(),
     install_requires=[
         # put your dependencies here, e.g. 'numpy', 'torch', ...

6f0948d8f431ce81492a36d4a5db648352d40229
gstrampelli <strampelligiovanni@gmail.com>
2025-06-16 15:01:38 -0400

    initial commitment

---
+++ README.rst
@@ -0,0 +1,15 @@
+##############
+StraKLIP 🚀🪐
+##############
+
+.. image:: logo.png
+
+StraKLIP is a data reduction pipeline for HST wide-field imaging. The current release has been tested with ACS, WFC3-IR and UVIS data.
+
+Installation Instructions: `here <https://straklip.readthedocs.io/en/latest/Installation-and-dependencies.html>`_
+
+Documentation: `here <https://straklip.readthedocs.io/en/latest/>`_
+
+Compatible test data:
+
+
--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
+# from setuptools import setup, find_packages
+#
+# setup(
+#     name='StraKLIP',
+#     version='1.1.1',
+#     packages=find_packages(),
+#     install_requires=[
+#         # put your dependencies here, e.g. 'numpy', 'torch', ...
+#     ],
+# )
+
 from setuptools import setup, find_packages
 
-setup(
-    name='StraKLIP',
-    version='1.1.1',
-    packages=find_packages(),
-    install_requires=[
-        # put your dependencies here, e.g. 'numpy', 'torch', ...
-    ],
-)
+setup(packages=find_packages(),
+      use_scm_version=True,
+      setup_requires=['setuptools_scm'],
+      )
